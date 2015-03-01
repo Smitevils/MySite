de3bc633de8153f916f499a059557d9337474eb0
Alexey <SmiteVils@yandex.ru>
2015-03-01 23:43:05 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SmiteVils - Фрилансер, Веб программист и просто человек у которого много хобби.</title>
+	<meta name="description" content="Сайтик фрилансера. Куча разнообразных проектов и хобби. Здесь мое резюме и контакты.">
+	<meta name="keywords" content="верстка, разработка, создание, сайтов, верстальщик, программист, web, coder, programmist, html5, css3, JQuery, JS, php.">
+	<!-- Прошу почаще индексировать, плиз! -->
+	<meta name="Document-state" content="Dynamic">
+	<meta name="revisit-after" content="3 days">
+	<!-- /Прошу почаще индексировать, плиз! -->
+	<meta name="author" content="smitevils.ru">
+</head>
+<body>
+
+</body>
+</html>
